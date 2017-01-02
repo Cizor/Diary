e139434dcf62966adf48b5b0cef02e351f128eb1
Amrit <amrit.srivastava@wipro.com>
2017-01-02 15:58:04 +0530

    TwoSum-testdome problem with vector pair
    Problem Statement:
    
    Write a function that, given a vector and a target sum, returns zero-based indices of any two distinct elements whose sum is equal to the target sum. If there are no such elements, the function should return (-1, -1).
    
    For example, findTwoSum({ 1, 3, 5, 7, 9 }, 12) should return any of the following pairs of indices:
    
    1, 4 (3 + 9 = 12)
    2, 3 (5 + 7 = 12)
    3, 2 (7 + 5 = 12)
    4, 1 (9 + 3 = 12)

---
+++ C++/TwoSum-vector-pair.cpp
@@ -0,0 +1,47 @@
+#include <stdexcept>
+#include <iostream>
+#include <vector>
+#include <utility>
+
+class TwoSum
+{
+	public:
+		static std::vector<std::pair<int, int> > findTwoSum(const std::vector<int>& list, int sum)
+		{
+			std::pair<int,int> p;
+			std::vector<std::pair<int,int> > vTemp;
+			size_t i = 0,j = 0;
+
+			for(i = 0;i < list.size();++i){
+				for(j = 0;j < list.size();j++){
+					if(list[i] + list[j] == sum){
+						p = std::make_pair(list[i],list[j]);
+						vTemp.push_back(p);
+					}
+				}
+			}
+			
+			return vTemp;
+		}
+};
+
+#ifndef RunTests
+int main()
+{
+	std::vector<int> list;
+	std::vector<std::pair<int,int> > v;
+	std::vector<std::pair<int, int> >::iterator i;
+
+	list.push_back(1);
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(7);
+	list.push_back(9);
+
+	v = TwoSum::findTwoSum(list, 8);
+
+	for(i = v.begin();i != v.end(); ++i){
+		std::cout<<i->first<<", "<<i->second<<std::endl;
+	}
+}
+#endif
